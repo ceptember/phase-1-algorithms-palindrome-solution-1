c722ffa4bbf2414176d6d142aaaf5e627a0e1128
Christy Perozzi <christy.perozzi@gmail.com>
2021-11-10 22:20:44 -0500

    commit tree dc778f69fbc2b5d16074cf3178983b1ba87e0ce0

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+function reverseString(word){
+  let wordArr = word.split('');
+  let reversedArr = wordArr.reverse();
+  return reversedArr.join(''); 
+}
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word == reverseString(word);
 }
 
 /* 
   Add your pseudocode here
+  array = word
+  reversed array = array.reverse()
+  new string = joined array
+  if new string == word, return true
+  else return false 
 */
 
 /*
   Add written explanation of your solution here
+
+  Convert the input string to an array so we can use JS's reverse method on it. 
+  After reversing, convert it back to a string and compare it to the original input. 
+  If they're equal, then it's a palendrome 
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,11 +32,14 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("noon"));
+  console.log("=>", isPalindrome("a"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("moon"));
 }
 
 module.exports = isPalindrome;
